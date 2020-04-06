e44a8fe8da7b31319c2bf16cfbd16d325d225b2f
JaimeLoaiza <jaimeloaiza@hotmail.es>
2020-04-06 13:44:03 -0500

    Modularidad-Encapsulamiento

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicoModularidadyEncapsulamiento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Empleados empleado = new Empleados();
+            empleado.Edad = 8;
+            empleado.Nombre = "Salomon Loaiza Gutierrez";
+            empleado.SueldoDiario = 193600.00m;
+            decimal total;
+            total = empleado.CalcularSalario(30);
+            Console.WriteLine("El Salario mensual del Empleado " + empleado.Nombre);
+            Console.WriteLine("es: " + total.ToString("C"));
+            Console.ReadKey();
+        }
+    }
+}
